18642fb139d0651cb9eccc8227a0444d42d132bd
onggi <hsk1533@naver.com>
2022-05-06 20:25:46 +0900

    load Data and graph build code

--- module.py
+++ module.py
@@ -1,9 +1,18 @@
 import FinanceDataReader as fdr
 import matplotlib.pyplot as plt
 
+# Load data
+def loadData(code,start=None,end=None):
+    data = fdr.DataReader(code,start,end)
+    return data
 
-samsung = fdr.DataReader('005930','20220301')
-kospi = fdr.DataReader('KS11',"20220301")
-plt.plot(kospi['Close'])
-plt.plot(samsung['Close'],'r')
-plt.show()
+def graphBuild(data,category):
+    plt.plot(data[category])
+    plt.show()
+
+def similarityMeasurement(data1, data2):
+    return 
+
+if __name__ == "__main__":
+    data = loadData('KS11','20220301')
+    graphBuild(data, 'Close')
